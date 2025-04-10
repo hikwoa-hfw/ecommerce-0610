0356215a2378ba3f71e2dd66f6d9a8f3bd5fd2f2
hikwoa-hfw <hibbanhfw@gmail.com>
2025-04-10 14:12:41 +0700

    feat: add register

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import express from "express"
-import cors from "cors"
-import { errorMiddleware } from "./middleware/error.middleware"
-import sampleRouter from "./routes/sample.router"
+import express from "express";
+import cors from "cors";
+import { errorMiddleware } from "./middleware/error.middleware";
+import sampleRouter from "./routes/sample.router";
+import authRouter from "./routes/auth.router";
 
-const app = express()
-app.use(cors())
-app.use(errorMiddleware)
+const app = express();
 
-app.use("/samples", sampleRouter)
+app.use(cors());
+app.use(express.json());
+
+app.use("/samples", sampleRouter);
+app.use("/auth", authRouter);
+
+app.use(errorMiddleware);
 
 app.listen(8000, () => {
-    console.log(`server running on PORT: ${8000}`)
-})
+  console.log(`server running on PORT: ${8000}`);
+});
