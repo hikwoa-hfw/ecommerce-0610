6e70066da7f6f232739b8e9b916c8d4a7c7aa098
hikwoa-hfw <hibbanhfw@gmail.com>
2025-04-10 11:32:37 +0700

    setup db connection

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(errorMiddleware)
 
-app.use("samples", sampleRouter)
+app.use("/samples", sampleRouter)
 
 app.listen(8000, () => {
     console.log(`server running on PORT: ${8000}`)
