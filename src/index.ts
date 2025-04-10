5b50e75b14151ed95514b5bde57e37a6121206a1
Hibban Faruq W <hibbanfaruq@mail.ugm.ac.id>
2025-04-10 11:06:52 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import { errorMiddleware } from "./middleware/error.middleware"
+import sampleRouter from "./routes/sample.router"
+
+const app = express()
+app.use(cors())
+app.use(errorMiddleware)
+
+app.use("samples", sampleRouter)
+
+app.listen(8000, () => {
+    console.log(`server running on PORT: ${8000}`)
+})
