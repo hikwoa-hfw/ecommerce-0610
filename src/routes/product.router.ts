c0cedf67dc7acb8ff4f1f653f36dc25ea03ea58f
Hibban Faruq W <hibbanhfw@gmail.com>
2025-04-16 11:58:24 +0700

    feat: update product (#4)

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { getSamplesController } from "../controllers/sample.controller";
-import { createProductController } from "../controllers/product.controller";
+import {
+  createProductController,
+  updateProductController,
+} from "../controllers/product.controller";
 import { verifyToken } from "../lib/jwt";
 import { verifyRole } from "../middleware/role.middleware";
 
-const router = Router()
+const router = Router();
 
-router.post("/", verifyToken, verifyRole(["ADMIN"]), createProductController)
+router.post("/", verifyToken, verifyRole(["ADMIN"]), createProductController);
+router.patch(
+  "/:id",
+  verifyToken,
+  verifyRole(["ADMIN"]),
+  updateProductController
+);
 
-export default router
+export default router;
---
+++ src/services/product/update-product.service.ts
@@ -0,0 +1,31 @@
+import { Product } from "@prisma/client";
+import prisma from "../../config/prisma";
+import { ApiError } from "../../utils/api-error";
+
+export const updateProductService = async (
+  id: number,
+  body: Partial<Product>
+) => {
+  const product = await prisma.product.findFirst({
+    where: { id },
+  });
+
+  if (!product) {
+    throw new ApiError("Product doesn't exist", 400);
+  }
+
+  if (body.name){
+    const existingProduct =await prisma.product.findFirst({
+        where:{name: body.name}
+    })
+
+    if (existingProduct){
+        throw new ApiError("Product name already exist", 400);
+    }
+  }
+
+  return await prisma.product.update({
+    where : {id},
+    data: body
+  })
+};
